c4cc42ac36643a4a9471158c42113c53be1f462c
hvelarde <hector.velarde@gmail.com>
2014-07-02 14:52:33 -0300

    Back to development: 1.0b3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0b2'
+version = '1.0b3.dev0'
 description = 'Intranet Modelo do Programa Interlegis.'
 long_description = (
     open('README.rst').read() + '\n' +
