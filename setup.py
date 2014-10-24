98eae4ace265fc39ba2be72b646943edec51ad92
Jean Ferri <jeanferri@interlegis.leg.br>
2014-10-24 20:02:31 -0200

    Back to development: 1.0b5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0b4'
+version = '1.0b5.dev0'
 description = 'Intranet Modelo do Programa Interlegis.'
 long_description = (
     open('README.rst').read() + '\n' +
